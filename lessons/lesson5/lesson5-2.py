6ef44a6b5b55d3ba2273b77315716d98365e724d
converse3 <g.budarin@list.ru>
2021-12-30 21:31:16 +0600

    Completed lesson 5

---
+++ lessons/lesson5/lesson5-2.py
@@ -0,0 +1,29 @@
+names = ['Кеша', 'Толик', 'Попугай']
+
+names.append('Попугай1')
+print(names)
+
+names.pop()
+print(names)
+
+n = names.index('Кеша')
+print(n)
+
+a = len(names)
+print(a)
+
+numbers = [4, 6, 81, 95, 108, 56]
+numbers.sort(reverse=True)
+print(numbers)
+
+numbers[4] = 5;
+print(numbers)
+
+strings = ['dsfdsf', 'jhghgfdh', 'ytjhgfjnv', 'aaaa', 'dfgyjh']
+strings.sort()
+print(strings)
+
+nums = [58, 51, 5444, 58.1, 74.7, 35.8]
+nums.sort()
+print(nums)
+
